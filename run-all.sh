058decf5fbe104f9b92eda962d5945cc7130df05
Simon Willison <swillison@gmail.com>
2022-12-03 12:28:56 -0800

    Run all Cargo ones too, refs #4, #2

--- run-all.sh
+++ run-all.sh
@@ -2,6 +2,7 @@
 
 # Use the find command to search for Justfiles in the current directory and its subdirectories
 justfiles=$(find . -name "Justfile")
+cargos=$(find . -name "Cargo.toml")
 
 # Save the current directory so we can change back to it later
 orig_dir=$(pwd)
@@ -16,6 +17,21 @@ for justfile in $justfiles; do
 
   # Run just without any arguments
   just
+
+  # Change back to the original directory
+  popd
+done
+
+# Loop through each Cargo.toml
+for cargo in $cargos; do
+  # Get the directory containing the Cargo.toml
+  dir=$(dirname $cargo)
+
+  # Change to the directory containing the Cargo.toml
+  pushd $dir
+
+  # Run cargo
+  cargo run
 
   # Change back to the original directory
   popd
