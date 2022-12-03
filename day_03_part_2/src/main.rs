ad63a5862ac655cc6f2878b9c5f1b2b077e6eb11
Simon Willison <swillison@gmail.com>
2022-12-03 13:07:44 -0800

    Refactored line reading, refs #4

--- day_03_part_2/src/main.rs
+++ day_03_part_2/src/main.rs
@@ -12,39 +12,43 @@ fn main() -> io::Result<()> {
     let mut lines = reader.lines();
     let mut found_char = 'X';
 
-    while let Some(Ok(line1)) = lines.next() {
-        if let Some(Ok(line2)) = lines.next() {
-            if let Some(Ok(line3)) = lines.next() {
-                // What character is present in all three lines?
-                let mut found = false;
-                for c1 in line1.chars() {
-                    for c2 in line2.chars() {
-                        for c3 in line3.chars() {
-                            if c1 == c2 && c2 == c3 {
-                                found = true;
-                                found_char = c1;
-                            }
-                        }
-                        if found {
-                            break;
-                        }
-                    }
-                    if found {
-                        break;
+    loop {
+        // Try this but break if it is an error:
+        let line1 = match lines.next() {
+            Some(Ok(line)) => line,
+            _ => break,
+        };
+        let line2 = lines.next().unwrap()?;
+        let line3 = lines.next().unwrap()?;
+
+        // What character is present in all three lines?
+        let mut found = false;
+        for c1 in line1.chars() {
+            for c2 in line2.chars() {
+                for c3 in line3.chars() {
+                    if c1 == c2 && c2 == c3 {
+                        found = true;
+                        found_char = c1;
                     }
                 }
                 if found {
-                    let priority = match found_char {
-                        'a'..='z' => found_char as u8 - 'a' as u8 + 1,
-                        'A'..='Z' => found_char as u8 - 'A' as u8 + 27,
-                        _ => 0,
-                    } as i32;
-                    score += priority;
-                } else {
-                    // Quit with error
-                    panic!("No character found that was in all 3 lines");
+                    break;
                 }
             }
+            if found {
+                break;
+            }
+        }
+        if found {
+            let priority = match found_char {
+                'a'..='z' => found_char as u8 - 'a' as u8 + 1,
+                'A'..='Z' => found_char as u8 - 'A' as u8 + 27,
+                _ => 0,
+            } as i32;
+            score += priority;
+        } else {
+            // Quit with error
+            panic!("No character found that was in all 3 lines");
         }
     }
     println!("{}", score);
