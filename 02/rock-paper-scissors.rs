c1f2e5fbde151e4bc33578114055adb924515431
Simon Willison <swillison@gmail.com>
2022-12-03 08:05:46 -0800

    First attempt at day 2, refs #3

---
+++ 02/rock-paper-scissors.rs
@@ -0,0 +1,38 @@
+use std::fs;
+use std::io;
+use std::io::BufRead;
+fn main() -> io::Result<()> {
+    let file = fs::File::open("input.txt")?;
+    let reader = io::BufReader::new(file);
+
+    let mut score = 0;
+
+    for line in reader.lines() {
+        let line = line?;
+
+        // Line is of form "opponent me" - split on the space
+        let mut parts = line.split(" ");
+        let opponent = parts.next().unwrap();
+        let me = parts.next().unwrap();
+        
+        // Who won? Opponent A beats me Z, B beats me X, C beats me Y
+        let win_score = match (opponent, me) {
+            ("A", "Z") | ("B", "X") | ("C", "Y") => 0,
+            ("A", "X") | ("B", "Y") | ("C", "Z") => 6,
+            _ => 3,
+        };
+        score += win_score;
+        // And an extra score based on what I picked
+        // X = 1, Y = 2, Z = 3 - use match for that too:
+        let extra_score = match me {
+            "X" => 1,
+            "Y" => 2,
+            "Z" => 3,
+            _ => 0,
+        };
+        println!("{} {} {} {}", opponent, me, win_score, extra_score);
+        score += extra_score;
+    }
+    println!("{}", score);
+    Ok(())
+}
