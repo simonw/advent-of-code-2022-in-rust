58aae1d112fc0f117e76ad543e7a3bd191137109
Simon Willison <swillison@gmail.com>
2022-12-09 08:53:07 -0800

    Day 9 part 2, closes #11

--- day_09/src/main.rs
+++ day_09/src/main.rs
@@ -3,12 +3,14 @@ use std::fs;
 fn main() {
     let file_contents = fs::read_to_string("input.txt").unwrap();
 
-    let mut head_pos: (i32, i32) = (0, 0);
-    let mut tail_pos: (i32, i32) = (0, 0);
-    let mut tail_visited = Vec::new();
-    tail_visited.push(tail_pos);
+    let mut knots: Vec<(i32, i32)> = Vec::new();
+    for _ in 0..10 {
+        knots.push((0, 0));
+    }
+    let mut tail_visited: Vec<(i32, i32)> = Vec::new();
+    tail_visited.push((0, 0));
 
-    print_board(&head_pos, &tail_pos);
+    // print_board(&knots);
 
     for line in file_contents.lines() {
         // Line is "R 4" - split into move, distance (integer)
@@ -19,42 +21,49 @@ fn main() {
         // First move the head
         for _ in 0..distance {
             match direction {
-                "R" => head_pos.0 += 1,
-                "L" => head_pos.0 -= 1,
-                "U" => head_pos.1 += 1,
-                "D" => head_pos.1 -= 1,
+                "R" => knots[0].0 += 1,
+                "L" => knots[0].0 -= 1,
+                "U" => knots[0].1 += 1,
+                "D" => knots[0].1 -= 1,
                 _ => panic!("Unknown direction {}", direction),
             }
-            println!("  head_pos = {:?}", head_pos);
-            // Now move the tail, if it needs to be moved
-            // Is tail within one of head in either direction?
-            if (head_pos.0 == tail_pos.0 && (head_pos.1 - tail_pos.1).abs() == 1)
-                || (head_pos.1 == tail_pos.1 && (head_pos.0 - tail_pos.0).abs() == 1)
-            {
-                println!("  Touching, no need to move");
-            }
-            // Are tail and head diagonally touching?
-            else if (head_pos.0 - tail_pos.0).abs() == 1 && (head_pos.1 - tail_pos.1).abs() == 1 {
-                println!("  Diagonally touching, no need to move");
-            } else {
-                // Need to move the tail
-                // If tail is not in same row AND column as head...
-                if head_pos.0 != tail_pos.0 && head_pos.1 != tail_pos.1 {
-                    // Move it diagonally in the right direction
-                    tail_pos.0 += to_one(head_pos.0 - tail_pos.0);
-                    tail_pos.1 += to_one(head_pos.1 - tail_pos.1);
+            println!("  head_pos = {:?}", knots[0]);
+            // Now move each other knot in turn, if it needs to be moved
+            for i in 1..knots.len() {
+                // Is knot within one of head in either direction?
+                let mut knot = knots[i];
+                let prev = knots[i - 1];
+                if (prev.0 == knot.0 && (prev.1 - knot.1).abs() == 1)
+                    || (prev.1 == knot.1 && (prev.0 - knot.0).abs() == 1)
+                {
+                    println!("  Touching, no need to move");
+                }
+                // Are tail and head diagonally touching?
+                else if (prev.0 - knot.0).abs() == 1 && (prev.1 - knot.1).abs() == 1 {
+                    println!("  Diagonally touching, no need to move");
                 } else {
-                    // Move it in the right direction
-                    if head_pos.0 == tail_pos.0 {
-                        tail_pos.1 += to_one(head_pos.1 - tail_pos.1);
+                    // Need to move the tail
+                    // If tail is not in same row AND column as head...
+                    if prev.0 != knot.0 && prev.1 != knot.1 {
+                        // Move it diagonally in the right direction
+                        knot.0 += to_one(prev.0 - knot.0);
+                        knot.1 += to_one(prev.1 - knot.1);
                     } else {
-                        tail_pos.0 += to_one(head_pos.0 - tail_pos.0);
+                        // Move it in the right direction
+                        if prev.0 == knot.0 {
+                            knot.1 += to_one(prev.1 - knot.1);
+                        } else {
+                            knot.0 += to_one(prev.0 - knot.0);
+                        }
+                    }
+                    knots[i] = knot;
+                    println!("  knot = {:?}", knot);
+                    if i == 9 {
+                        tail_visited.push(knot);
                     }
                 }
-                println!("  tail_pos = {:?}", tail_pos);
-                tail_visited.push(tail_pos);
             }
-            print_board(&head_pos, &tail_pos);
+            // print_board(&head_pos, &tail_pos);
         }
     }
     println!("tail_visited = {:?}", tail_visited);
@@ -67,8 +76,8 @@ fn main() {
     }
     println!("Number of unique locations = {:?}", unique_tuples.len());
 }
-
-fn print_board(head_pos: &(i32, i32), tail_pos: &(i32, i32)) {
+/*
+fn print_board(knots: &knot) {
     for y in 0..10 {
         for x in 0..10 {
             // If both in same spot do an X
@@ -85,6 +94,7 @@ fn print_board(head_pos: &(i32, i32), tail_pos: &(i32, i32)) {
         println!("");
     }
 }
+ */
 
 
 fn to_one(n: i32) -> i32 {
