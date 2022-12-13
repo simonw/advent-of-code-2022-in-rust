93f7f79c16f54f2229aacaefba50e81b99fbc6b6
Simon Willison <swillison@gmail.com>
2022-12-13 06:34:00 -0800

    Parsed a single line, refs #15

--- day_13/src/main.rs
+++ day_13/src/main.rs
@@ -3,37 +3,68 @@ use std::fs;
 // A struct representing a nested list of lists of integers
 #[derive(Debug, Clone)]
 struct NestedInteger {
-    value: i32,
+    // value can be blank or an integer
+    value: Option<i32>,
     list: Vec<NestedInteger>,
 }
 
 fn main() {
-    let file_contents = fs::read_to_string("input.txt").unwrap();
+    let file_contents = fs::read_to_string("example1.txt").unwrap();
     let mut stack = Vec::new();
     for line in file_contents.lines() {
         // line is a JSON array e.g. [[1, 2], [3, 14]] - parse it
-        let mut current = NestedInteger { value: 0, list: Vec::new() };
+        let mut current = NestedInteger {
+            value: None,
+            list: Vec::new(),
+        };
+        let mut has_found_first = false;
+        let mut current_integer = 0;
+        let mut in_integer = false;
         for c in line.chars() {
             match c {
                 '[' => {
                     // Start of a new list
-                    stack.push(current);
-                    current = NestedInteger { value: 0, list: Vec::new() };
+                    if has_found_first {
+                        stack.push(current);
+                    }
+                    has_found_first = true;
+                    current = NestedInteger {
+                        value: None,
+                        list: Vec::new(),
+                    };
+                    current_integer = 0;
+                    in_integer = false;
                 }
                 ']' => {
                     // End of a list
-                    let mut parent = stack.pop().unwrap();
-                    parent.list.push(current);
-                    current = parent;
+                    if in_integer {
+                        current.list.push(NestedInteger {
+                            value: Some(current_integer),
+                            list: Vec::new(),
+                        });
+                    }
+                    if let Some(mut parent) = stack.pop() {
+                        parent.list.push(current.clone());
+                        current = parent;
+                    }
+                    current_integer = 0;
+                    in_integer = false;
                 }
                 ',' => {
                     // End of an integer
-                    current.list.push(current.clone());
-                    current = NestedInteger { value: 0, list: Vec::new() };
+                    if in_integer {
+                        current.list.push(NestedInteger {
+                            value: Some(current_integer),
+                            list: Vec::new(),
+                        });
+                        current_integer = 0;
+                        in_integer = false;
+                    }
                 }
                 '0'..='9' => {
                     // A digit
-                    current.value = current.value * 10 + (c as i32 - '0' as i32);
+                    current_integer = current_integer * 10 + (c as i32 - '0' as i32);
+                    in_integer = true;
                 }
                 _ => {}
             }
