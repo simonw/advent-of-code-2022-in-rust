3dfa020d36cc0dace893259d07e841de35e4018e
Simon Willison <swillison@gmail.com>
2022-12-13 22:10:23 -0800

    Solution for Day 13 part 1, refs #15

--- day_13/src/main.rs
+++ day_13/src/main.rs
@@ -1,17 +1,50 @@
 use parameterized::parameterized;
+use std::cmp::{Ord, Ordering, PartialOrd};
 use std::fs;
 
-// A struct representing a nested list of lists of integers
-#[derive(Debug, Clone, Eq, PartialOrd)]
-struct NestedInteger {
-    // value can be blank or an integer
-    value: Option<i32>,
-    list: Vec<NestedInteger>,
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum NestedInteger {
+    Value(i32),
+    Children(Vec<NestedInteger>),
 }
 
-impl PartialEq for NestedInteger {
-    fn eq(&self, other: &Self) -> bool {
-        self.value == other.value && self.list == other.list
+impl Ord for NestedInteger {
+    fn cmp(&self, other: &Self) -> Ordering {
+        println!("Comparing {:?} and {:?}", self, other);
+        match (self, other) {
+            (NestedInteger::Value(a), NestedInteger::Value(b)) => a.cmp(b),
+            // If one side is a value and the other a list, compare to [value] - in both directions:
+            (NestedInteger::Value(_), _) => {
+                NestedInteger::Children(vec![self.clone()]).cmp(other)
+            }
+            (_, NestedInteger::Value(_)) => {
+                self.cmp(&NestedInteger::Children(vec![other.clone()]))
+            }
+            // If both sides are lists, compare them one element at a time
+            (NestedInteger::Children(a), NestedInteger::Children(b)) => {
+                let mut a = a.iter();
+                let mut b = b.iter();
+                loop {
+                    println!("  a: {:?}, b: {:?}", a, b);
+                    match (a.next(), b.next()) {
+                        (Some(a), Some(b)) => match a.cmp(b) {
+                            Ordering::Equal => continue,
+                            other => return other,
+                        },
+                        // If the right list runs out of items first, the inputs are not in the right order
+                        (Some(_), None) => return Ordering::Greater,
+                        (None, Some(_)) => return Ordering::Less,
+                        (None, None) => return Ordering::Equal,
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl PartialOrd for NestedInteger {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
 
@@ -55,15 +88,13 @@ fn main() {
 
 fn parse_line(line: &str) -> NestedInteger {
     // line is a JSON array e.g. [[1, 2], [3, 14]] - parse it
+    let mut chars = line.chars();
+    let mut current = NestedInteger::Children(Vec::new());
     let mut stack = Vec::new();
-    let mut current = NestedInteger {
-        value: None,
-        list: Vec::new(),
-    };
-    let mut has_found_first = false;
     let mut current_integer = 0;
     let mut in_integer = false;
-    for c in line.chars() {
+    let mut has_found_first = false;
+    while let Some(c) = chars.next() {
         match c {
             '[' => {
                 // Start of a new list
@@ -71,23 +102,28 @@ fn parse_line(line: &str) -> NestedInteger {
                     stack.push(current);
                 }
                 has_found_first = true;
-                current = NestedInteger {
-                    value: None,
-                    list: Vec::new(),
-                };
+                current = NestedInteger::Children(Vec::new());
                 current_integer = 0;
                 in_integer = false;
             }
             ']' => {
                 // End of a list
                 if in_integer {
-                    current.list.push(NestedInteger {
-                        value: Some(current_integer),
-                        list: Vec::new(),
-                    });
+                    // Push onto Children list in current
+                    match &mut current {
+                        NestedInteger::Children(children) => {
+                            children.push(NestedInteger::Value(current_integer));
+                        }
+                        _ => panic!("Unexpected type"),
+                    }
                 }
                 if let Some(mut parent) = stack.pop() {
-                    parent.list.push(current.clone());
+                    match &mut parent {
+                        NestedInteger::Children(children) => {
+                            children.push(current.clone());
+                        }
+                        _ => panic!("Unexpected type"),
+                    }
                     current = parent;
                 }
                 current_integer = 0;
@@ -96,10 +132,12 @@ fn parse_line(line: &str) -> NestedInteger {
             ',' => {
                 // End of an integer
                 if in_integer {
-                    current.list.push(NestedInteger {
-                        value: Some(current_integer),
-                        list: Vec::new(),
-                    });
+                    match &mut current {
+                        NestedInteger::Children(children) => {
+                            children.push(NestedInteger::Value(current_integer));
+                        }
+                        _ => panic!("Unexpected type"),
+                    }
                     current_integer = 0;
                     in_integer = false;
                 }
@@ -115,17 +153,75 @@ fn parse_line(line: &str) -> NestedInteger {
     current
 }
 
+/*
+
+fn old_parse_ine(line: &str) -> NestedInteger {
+    let mut stack = Vec::new();
+    let mut current;
+    let mut has_found_first = false;
+    let mut current_integer = 0;
+    let mut in_integer = false;
+    for c in line.chars() {
+        match c {
+            '[' => {
+                // Start of a new list
+                if has_found_first {
+                    stack.push(current);
+                }
+                has_found_first = true;
+                current = NestedInteger::Children(Vec::new());
+                current_integer = 0;
+                in_integer = false;
+            }
+            ']' => {
+                // End of a list
+                if in_integer {
+                    // Push onto Children list in current
+                    match &mut current {
+                        NestedInteger::Children(children) => {
+                            children.push(NestedInteger::Value(current_integer));
+                        }
+                        _ => panic!("Unexpected type"),
+                    }
+                }
+                if let Some(mut parent) = stack.pop() {
+                    parent.push(current.clone());
+                    current = parent;
+                }
+                current_integer = 0;
+                in_integer = false;
+            }
+            ',' => {
+                // End of an integer
+                if in_integer {
+                    current.list.push(NestedInteger::Value(current_integer));
+                    current_integer = 0;
+                    in_integer = false;
+                }
+            }
+            '0'..='9' => {
+                // A digit
+                current_integer = current_integer * 10 + (c as i32 - '0' as i32);
+                in_integer = true;
+            }
+            _ => {}
+        }
+    }
+    current
+}
+*/
+
 #[test]
 fn test_parse_line() {
     let line = "[[1, 2], [3, 14]]";
     let parsed = parse_line(line);
-    assert_eq!(parsed.list.len(), 2);
-    assert_eq!(parsed.list[0].list.len(), 2);
-    assert_eq!(parsed.list[0].list[0].value, Some(1));
-    assert_eq!(parsed.list[0].list[1].value, Some(2));
-    assert_eq!(parsed.list[1].list.len(), 2);
-    assert_eq!(parsed.list[1].list[0].value, Some(3));
-    assert_eq!(parsed.list[1].list[1].value, Some(14));
+    assert_eq!(
+        parsed,
+        NestedInteger::Children(vec![
+            NestedInteger::Children(vec![NestedInteger::Value(1), NestedInteger::Value(2)]),
+            NestedInteger::Children(vec![NestedInteger::Value(3), NestedInteger::Value(14)])
+        ])
+    );
 }
 
 #[test]
@@ -144,6 +240,9 @@ fn test_order_comparison(left: &str, right: &str) {
     // For each example, the one on the left should be < the one on the right
     let parsed_left = parse_line(left);
     let parsed_right = parse_line(right);
+    println!("Left: {:?}", parsed_left);
+    println!("Right: {:?}", parsed_right);
+
     assert_eq!(
         parsed_left < parsed_right,
         true,
